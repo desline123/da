7c02999e3a21c3747c2f2ced3e9a29ce55664979
desline123 <maximrus2019@gmail.com>
2026-05-25 09:58:27 +0300

    Delete db.php

--- db.php
+++
@@ -1,5 +0,0 @@
-<?php
-$con = mysqli_connect('MySQL-5.7', 'root', '', 'service_quality');
-if(!$con) die('Ошибка подключения к базе данных: ' . mysqli_connect_error());
-mysqli_set_charset($con, 'utf8');
-?>
